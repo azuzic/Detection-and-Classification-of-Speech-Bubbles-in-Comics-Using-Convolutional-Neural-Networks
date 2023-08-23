38b119c294056738363169f08383fcf4e47a95f3
Alesandro Zuzic <azuzic@student.unipu.hr>
2023-08-23 15:34:48 +0200

    Switched to easyOCR

--- main/image_manipulation.py
+++ main/image_manipulation.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytesseract
 import cv2
 import math
+import easyocr
 
 image_size = 800
 empty_image = Image.new("RGBA", (image_size, image_size), (0, 0, 0, 0))
@@ -68,19 +69,28 @@ def group_boxes(boxes):
     grouped_boxes = []
     
     while boxes:
-        box = boxes[0]
-        x1, y1, x2, y2 = map(int, box.split()[1:5])
-        combined_text = box.split()[0]
+        bbox, text, prob = boxes[0]
+        print(bbox)
+        x_points, y_points = zip(*bbox)
+
+        x1, x2 = min(x_points), max(x_points)
+        y1, y2 = min(y_points), max(y_points)
+        combined_text = text
         
         del boxes[0]
         
         i = 0
         while i < len(boxes):
-            x1_i, y1_i, x2_i, y2_i = map(int, boxes[i].split()[1:5])
+            (bbox_i, text_i, _), _, _ = boxes[i]
+            x_points_i, y_points_i = zip(*bbox_i)
+
+            x1_i, x2_i = min(x_points_i), max(x_points_i)
+            y1_i, y2_i = min(y_points_i), max(y_points_i)
+            
             overlap = (x1 < x2_i) and (x2 > x1_i) and (y1 < y2_i) and (y2 > y1_i)
             
             if overlap:
-                combined_text += ' ' + boxes[i].split()[0]
+                combined_text += ' ' + text_i
                 x1 = min(x1, x1_i)
                 x2 = max(x2, x2_i)
                 y1 = min(y1, y1_i)
@@ -89,62 +99,11 @@ def group_boxes(boxes):
             else:
                 i += 1
         
-        grouped_boxes.append((x1, y1, x2, y2, combined_text))
+        grouped_boxes.append((((x1, y1), (x2, y2)), combined_text))
     
-    return combine_close_boxes(grouped_boxes)
+    return grouped_boxes
 
-def combine_close_boxes(grouped_boxes, distance_threshold = 10):
-    combined_boxes = []
-    
-    while grouped_boxes:
-        box = grouped_boxes[0]
-        x1, y1, x2, y2, text = box
-        
-        del grouped_boxes[0]
-        
-        i = 0
-        while i < len(grouped_boxes):
-            x1_i, y1_i, x2_i, y2_i, text_i = grouped_boxes[i]
-            distance = abs((y2 + y1) - (y2_i + y1_i))
-            
-            if distance < distance_threshold:
-                combined_text = text + ' ' + text_i
-                x1 = min(x1, x1_i)
-                x2 = max(x2, x2_i)
-                y1 = min(y1, y1_i)
-                y2 = max(y2, y2_i)
-                text = combined_text
-                del grouped_boxes[i]
-            else:
-                i += 1
-        
-        combined_boxes.append((x1, y1, x2, y2, text))
-    
-    return combined_boxes
 
-def divide_image_into_grid(image, piece_size):
-    height, width = image.shape
-    pieces = []
-    
-    for y in range(0, height, piece_size):
-        for x in range(0, width, piece_size):
-            piece = image[y:y+piece_size, x:x+piece_size]
-            pieces.append(piece)
-    
-    return pieces
-
-def divide_image_into_grid_color(image, piece_size):
-    height, width, _ = image.shape
-    pieces = []
-    
-    for y in range(0, height, piece_size):
-        for x in range(0, width, piece_size):
-            piece = image[y:y+piece_size, x:x+piece_size]
-            pieces.append(piece)
-    
-    return pieces
-
-def reconstruct_image_from_grid_color(piece_list, piece_size, image_shape):
     image_height, image_width = image_shape.shape
     reconstructed_image = Image.new('RGB', (image_width, image_height))
     
@@ -162,31 +121,32 @@ def reconstruct_image_from_grid_color(piece_list, piece_size, image_shape):
 def detect_and_draw_speech_bubbles(self, pixmap):
     global empty_image
     
-    # Convert PIL image to cv2 image
-    cv_image = cv2.cvtColor(np.array(empty_image), cv2.COLOR_RGB2BGR)
-    
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-    
-    piecesColor = divide_image_into_grid_color(cv_image, 100)
-    pieces = divide_image_into_grid(gray_image, 100)
-    
-    for piece, pieceColor in zip(pieces, piecesColor):
-        # Apply OCR to get bounding box information and text for each word
-        boxes = pytesseract.image_to_boxes(piece, config='--psm 6')
-
-        grouped_boxes = group_boxes(boxes.splitlines())
+    try:
+        cv_image = cv2.cvtColor(np.array(empty_image), cv2.COLOR_RGB2BGR)
         
-        for box in grouped_boxes:
-            x1, y1, x2, y2, text = box
-            cv2.rectangle(pieceColor, (x1, piece.shape[0] - y1), (x2, piece.shape[0] - y2), (0, 0, 255), 1)
-            cv2.putText(pieceColor, text, (x1, piece.shape[0] - y1 + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-        
-        empty_image = Image.fromarray(cv2.cvtColor(pieceColor, cv2.COLOR_BGR2RGB))
-        resizeImage(self, pixmap)
-        QApplication.processEvents()
+        gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-    empty_image = reconstruct_image_from_grid_color(piecesColor, 100, gray_image)
+        reader = easyocr.Reader(lang_list=['en'])  # Specify the language(s) you want to detect
+
+        results = reader.readtext(gray_image)
+
+        grouped_boxes = group_boxes(results)
+
+        for (bbox, text) in grouped_boxes:
+            x_points, y_points = zip(*bbox)
+            x1, x2 = min(x_points), max(x_points)
+            y1, y2 = min(y_points), max(y_points)
+            cv2.rectangle(cv_image, (x1, y1), (x2, y2), (0, 0, 255), 1)
+            cv2.putText(cv_image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+            empty_image = Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
+            resizeImage(self, pixmap)
+            QApplication.processEvents()
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
 
 
 
